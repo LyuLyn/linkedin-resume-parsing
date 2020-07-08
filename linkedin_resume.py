762c018c64b1c4c491306fa275905b2fa4672f38
JoeyLyu <lvliangxiong@foxmail.com>
2020-07-08 23:25:55 +0800

    add batch file processing, and bug fix

--- linkedin_resume.py
+++ linkedin_resume.py
@@ -7,6 +7,8 @@ import sys
 import glob
 import datetime
 import json
+import pandas as pd
+from pandas import ExcelWriter
 from collections import OrderedDict
 from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfdocument import PDFDocument
@@ -55,6 +57,9 @@ EOL = ['\n', '\r\n', '\r']
 
 CLEAN_CHAR = ['\xa0']
 
+MONTH = {'January': "01", 'Feburary': "02", 'March': "03", 'April': "04", 'May': "05", 'June': "06",
+         'July': "07", 'August': "08", 'September': "09", 'October': "10", 'November': "11", 'December': "12"}
+
 
 def getfilelist(path, extension='.pdf'):
     """Returns a list of files in a given path
@@ -67,6 +72,7 @@ def getfilelist(path, extension='.pdf'):
         list: a list of files in a given path
     """
     filenames = []
+    # pylint: disable=unused-variable
     for root, dirs, files in os.walk(path):
         for file in files:
             if (extension):
@@ -176,7 +182,8 @@ class LinkedInResume:
             bool: True indicates the given obj is or contains the header
         """
         font = self.get_font(obj)
-        return self.check_font_size(font, SECTION_HEAD_FONTSIZE_MAIN, 0.5) and obj.get_text().startswith(name)
+        return self.check_font_size(font, SECTION_HEAD_FONTSIZE_MAIN, 0.5) \
+            and obj.get_text().startswith(name)
 
     def get_section_name(self, obj):
         """Extract the section header name from a given obj,
@@ -206,7 +213,7 @@ class LinkedInResume:
         """
         for eol in EOL:
             while string.endswith(eol):
-                string = string[:len(string) - 1]
+                string = string[:len(string) - len(eol)]
         return string
 
     def clean(self, string):
@@ -263,46 +270,55 @@ class LinkedInResume:
         return ret
 
     def parse_exp(self):
-        exp = self.box_to_text(self.sections["Experience"])
-        ret = []
+        if "Experience" in self.sections:
+            exp = self.box_to_text(self.sections["Experience"])
+            ret = []
 
-        index = 0
-        count = len(exp)
+            index = 0
+            count = len(exp)
 
-        while index < count:
-            obj = exp[index]
-            font = self.get_font(obj)
             # search for company
-            if self.check_font_size(font, EXP_COMPANY_FONT_SIZE, 0.5):
-                company = obj.get_text()
-                index = index + 1
-                # search for job title
-                while index < count:
-                    obj = exp[index]
-                    font = self.get_font(obj)
-                    if self.check_font_size(font, EXP_TITLE_FONT_SIZE, 0.5):
-                        title = obj.get_text()
-                        index = index + 1
-                        # search for job title
-                        while index < count:
-                            obj = exp[index]
-                            font = self.get_font(obj)
-                            if self.check_font_size(font, EXP_DURARION_FONT_SIZE, 0.5):
-                                duration = obj.get_text()
-                                ret.append(
-                                    {"company": self.clean(company),
-                                        "job_title": self.clean(title),
-                                        "duration":
-                                        self.parse_exp_duration(self.clean(duration))})
-                                break
-                            index = index + 1
-                    elif self.check_font_size(font, EXP_COMPANY_FONT_SIZE, 0.5):
-                        index = index - 1
-                        break
+            while index < count:
+                obj = exp[index]
+                font = self.get_font(obj)
+                if self.check_font_size(font, EXP_COMPANY_FONT_SIZE, 0.5):
+                    company = obj.get_text()
                     index = index + 1
-            index = index + 1
-        self.experience = ret
-        self.data['experience'] = self.experience
+                    # search for job title
+                    while index < count:
+                        obj = exp[index]
+                        font = self.get_font(obj)
+                        if self.check_font_size(font, EXP_TITLE_FONT_SIZE, 0.5):
+                            title = obj.get_text()
+                            index = index + 1
+                            # search for duration
+                            while index < count:
+                                obj = exp[index]
+                                font = self.get_font(obj)
+                                if self.check_font_size(font, EXP_DURARION_FONT_SIZE, 0.5):
+                                    duration = obj.get_text()
+                                    ret.append(
+                                        {"company": self.clean(company),
+                                            "job_title": self.clean(title),
+                                            "duration":
+                                            self.parse_exp_duration(self.clean(duration))})
+                                    break
+                                elif self.check_font_size(font, EXP_TITLE_FONT_SIZE, 0.5) or \
+                                        self.check_font_size(font, EXP_COMPANY_FONT_SIZE, 0.5):
+                                    ret.append(
+                                        {"company": self.clean(company),
+                                            "job_title": self.clean(title),
+                                            "duration": ""})
+                                    index = index - 1
+                                    break
+                                index = index + 1
+                        elif self.check_font_size(font, EXP_COMPANY_FONT_SIZE, 0.5):
+                            index = index - 1
+                            break
+                        index = index + 1
+                index = index + 1
+            self.experience = ret
+            self.data['experience'] = self.experience
 
     def parse_exp_duration(self, duration):
         """Parse experience date string like 'February 2019-Present(1 year 6 months)'
@@ -320,43 +336,63 @@ class LinkedInResume:
         if len(year_list) == 1:
             from_year = year_list[0]
             if ("present" in duration.lower()):
-                to_year = datetime.date.today().year
+                to_year = "Present"
         elif len(year_list) == 2:
             from_year = year_list[0]
             to_year = year_list[1]
-        return {"from_year": from_year, "to_year": to_year}
+
+        months = []
+        from_month = "01"
+        to_month = "01"
+
+        for month in MONTH:
+            if month in duration:
+                months.append(MONTH[month])
+
+        if len(months) == 0:
+            pass
+        elif len(months) == 1:
+            from_month = months[0]
+        else:
+            from_month = months[0]
+            to_month = months[1]
+
+        return {"from": from_year + "-" + from_month + "-01",
+                "to": to_year + "-" + to_month + "-01" if to_year != "Present" else to_year}
 
     def parse_edu(self):
-        edu = self.box_to_text(self.sections["Education"])
-        ret = []
+        if "Education" in self.sections:
+            edu = self.box_to_text(self.sections["Education"])
+            ret = []
 
-        index = 0
-        count = len(edu)
+            index = 0
+            count = len(edu)
 
-        while index < count:
-            obj = edu[index]
-            font = self.get_font(obj)
             # search for unversity
-            if self.check_font_size(font, EDU_UNIVERITY_FONT_SIZE, 0.5):
-                univerity = obj.get_text()
-                index = index + 1
-                # search for info
-                while index < count:
-                    obj = edu[index]
-                    font = self.get_font(obj)
-                    if self.check_font_size(font, EDU_INFO_FONT_SIZE, 0.5):
-                        info = obj.get_text()
-                        edu_item = {"university": self.clean(univerity)}
-                        edu_item.update(self.parse_edu_info(self.clean(info)))
-                        ret.append(edu_item)
-                        break
-                    elif self.check_font_size(font, EXP_COMPANY_FONT_SIZE, 0.5):
-                        index = index - 1
-                        break
+            while index < count:
+                obj = edu[index]
+                font = self.get_font(obj)
+                if self.check_font_size(font, EDU_UNIVERITY_FONT_SIZE, 0.5):
+                    univerity = obj.get_text()
                     index = index + 1
-            index = index + 1
-        self.education = ret
-        self.data['education'] = self.education
+                    # search for info
+                    while index < count:
+                        obj = edu[index]
+                        font = self.get_font(obj)
+                        if self.check_font_size(font, EDU_INFO_FONT_SIZE, 0.5):
+                            info = obj.get_text()
+                            edu_item = {"university": self.clean(univerity)}
+                            edu_item.update(
+                                self.parse_edu_info(self.clean(info)))
+                            ret.append(edu_item)
+                            break
+                        elif self.check_font_size(font, EDU_UNIVERITY_FONT_SIZE, 0.5):
+                            index = index - 1
+                            break
+                        index = index + 1
+                index = index + 1
+            self.education = ret
+            self.data['education'] = self.education
 
     def parse_edu_info(self, info):
         info_list = re.split(',|·', info)
@@ -365,8 +401,15 @@ class LinkedInResume:
         major = ''
         duration = {}
 
+        for i in info_list:
+            if i == '':
+                info_list.remove(i)
+
         if len(info_list) == 1:
-            degree = info
+            if ('·' in info):
+                duration = self.parse_edu_date(info_list[0])
+            else:
+                degree = info[0]
         elif len(info_list) == 2:
             if '·' in info:
                 degree = info_list[0]
@@ -377,7 +420,7 @@ class LinkedInResume:
         else:
             degree = info_list[0]
             major = info_list[1]
-            duration = self.parse_edu_date(info_list[2])
+            duration = self.parse_edu_date(info_list[-1])
         return {"degree": degree, "major": major, "duration": duration}
 
     def parse_edu_date(self, date):
@@ -403,13 +446,69 @@ class LinkedInResume:
         with open(filename, 'w') as f:
             json.dump(self.data, f)
 
-    def _parse(self):
+    def data_to_dataframe(self, data):
+        """Convert a list of dict data to DataFrame
+
+        Args:
+            data (list): a list of dict data
+        """
+        ret = []
+        for item in data:
+            dict_info = {"name": self.name}
+            for key, value in item.items():
+                if isinstance(value, dict):
+                    for inner_key, inner_value in value.items():
+                        dict_info[inner_key] = inner_value
+                else:
+                    dict_info[key] = value
+            ret.append(dict_info)
+        return pd.DataFrame(ret)
+
+    def backup_exp_edu_to_excel(self, filename="results.xlsx"):
+        exp_df = pd.DataFrame()
+        edu_df = pd.DataFrame()
+
+        # experience
+        if hasattr(self, 'experience'):
+            exp_df = self.data_to_dataframe(self.experience)
+
+        # education
+        if hasattr(self, 'experience'):
+            edu_df = self.data_to_dataframe(self.experience)
+
+        # pylint: disable=abstract-class-instantiated
+        with ExcelWriter(filename) as writer:
+            if(not exp_df.empty):
+                exp_df.to_excel(writer, sheet_name="experience")
+            if(not edu_df.empty):
+                edu_df.to_excel(writer, sheet_name="education")
+
+    def get_exp_df(self):
+        if hasattr(self, 'experience'):
+            return self.data_to_dataframe(self.experience)
+
+    def get_edu_df(self):
+        if hasattr(self, 'education'):
+            return self.data_to_dataframe(self.education)
+
+    def _parse_and_save(self):
+        self.parse_pages()
         self.split()
         self.parse_main_panel()
         self.parse_exp()
         self.parse_edu()
         self.backup_json()
-        pass
+        self.backup_exp_edu_to_excel()
+
+    def _parse(self):
+        self.parse_pages()
+        self.split()
+        self.parse_main_panel()
+        self.parse_exp()
+        self.parse_edu()
+
+    def parse_and_save(self):
+        self._parse_and_save()
 
     def parse(self):
         self._parse()
